e11655da4c582d818640825d2f3079e43ce0e2b8
Yuji Ichikawa <ichikawa.yuji@gmail.com>
2016-02-24 16:07:54 +0900

    initial

---
+++ package.js
@@ -0,0 +1,13 @@
+Package.describe({
+  name: 'new3rs:nico-comment-view',
+  summary: 'Nico-nico like comment view',
+  version: '1.0.0',
+  git: 'https://github.com/new3Rs/meteor-nico-comment-view.git'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.use('jquery', 'client');
+  api.addFiles('nico-comment-view.js', 'client');
+  api.addFiles('nico-comment-view.css', 'client');
+});
